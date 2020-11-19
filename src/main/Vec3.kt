30deecc02d9bdb14bca1d5db1e34a7878e4fa499
Daniel Lorch <dlorch@gmail.com>
2020-11-19 17:36:58 +0100

    Add tests for Vec3

---
+++ src/main/Vec3.kt
@@ -0,0 +1,63 @@
+package main
+
+import kotlin.math.sqrt
+
+class Vec3(e0: Double = 0.0, e1: Double = 0.0, e2: Double = 0.0) {
+    private var e = arrayOf(e0, e1, e2)
+    fun x() = e[0]
+    fun y() = e[1]
+    fun z() = e[2]
+    operator fun unaryMinus(): Vec3 = Vec3(-e[0], -e[1], -e[2])
+    // a[i] gets translated to a.get(i)
+    operator fun get(i: Int): Double = e[i]
+    // a += b gets translated to a.plusAssign(b)
+    operator fun plusAssign(v: Vec3) {
+        e[0] += v[0]
+        e[1] += v[1]
+        e[2] += v[2]
+    }
+    // a *= b gets translated to a.timesAssign(b)
+    operator fun timesAssign(t: Double) {
+        e[0] *= t
+        e[1] *= t
+        e[2] *= t
+    }
+    // a /= b gets translated to a.divAssign(b)
+    operator fun divAssign(t: Double) {
+        this *= 1 / t
+    }
+    fun length() = sqrt(lengthSquared())
+    fun lengthSquared() = e[0]*e[0] + e[1]*e[1] + e[2]*e[2]
+    override fun toString() = "" + e[0] + " " + e[1] + " " + e[2]
+    override fun equals(other: Any?): Boolean {
+        if(other is Vec3) {
+            return other.x() == e[0] && other.y() == e[1] && other.z() == e[2]
+        }
+        return false
+    }
+    override fun hashCode(): Int {
+        return e.hashCode()
+    }
+    // a + b translates to a.plus(b)
+    operator fun plus(v: Vec3) = Vec3(e[0] + v.x(), e[1] + v.y(), e[2] + v.z())
+    // a - b translates to a.minus(b)
+    operator fun minus(v: Vec3) = Vec3(e[0] - v.x(), e[1] - v.y(), e[2] - v.z())
+    // a * b translates to a.times(b)
+    operator fun times(v: Vec3) = Vec3(e[0] * v.x(), e[1] * v.y(), e[2] * v.z())
+    operator fun times(t: Double) = Vec3(e[0] * t, e[1] * t, e[2] * t)
+    // a / b translates to a.div(b)
+    operator fun div(t: Double) = this * (1/t)
+    fun dot(v: Vec3): Double =
+            e[0] * v.x() +
+            e[1] * v.y() +
+            e[2] * v.z()
+    fun cross(v: Vec3): Vec3 = Vec3(
+            e[1] * v.z() - e[2] * v.y(),
+            e[2] * v.x() - e[0] * v.z(),
+            e[0] * v.y() - e[1] * v.x()
+    )
+    fun unitVector(): Vec3 = this / this.length()
+}
+
+typealias Point3 = Vec3
+typealias Color = Vec3
---
+++ src/test/Vec3Test.kt
@@ -0,0 +1,105 @@
+package test
+
+import main.Vec3
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Vec3Test {
+    @Test
+    fun testUnaryMinus() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val result = -u
+        assertEquals(Vec3(-1.0, -2.0, -3.0), result)
+    }
+    @Test
+    fun testAccessElements() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        assertEquals(1.0, u[0])
+        assertEquals(2.0, u[1])
+        assertEquals(3.0, u[2])
+    }
+    @Test
+    fun testPlusAssign() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        u += v
+        val expected = Vec3(5.0, 7.0, 9.0)
+        assertEquals(expected, u)
+    }
+    @Test
+    fun testTimesAssign() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        u *= 3.0
+        val expected = Vec3(3.0, 6.0, 9.0)
+        assertEquals(expected, u)
+    }
+    @Test
+    fun testDivAssign() {
+        val u = Vec3(2.0, 4.0, 6.0)
+        u /= 2.0
+        val expected = Vec3(1.0, 2.0, 3.0)
+        assertEquals(expected, u)
+    }
+    @Test
+    fun testLength() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        assertEquals(3.7416573867739413, u.length())
+    }
+    @Test
+    fun testLengthSquared() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        assertEquals(14.0, u.lengthSquared())
+    }
+    @Test
+    fun testPlus() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        val expected = Vec3(5.0, 7.0, 9.0)
+        assertEquals(expected, u + v)
+    }
+    @Test
+    fun testMinus() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        val expected = Vec3(-3.0, -3.0, -3.0)
+        assertEquals(expected, u - v)
+    }
+    @Test
+    fun testTimes() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        val expected = Vec3(4.0, 10.0, 18.0)
+        assertEquals(expected, u * v)
+    }
+    @Test
+    fun testTimesScalar() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val expected = Vec3(2.0, 4.0, 6.0)
+        assertEquals(expected, u * 2.0)
+    }
+    @Test
+    fun testDiv() {
+        val u = Vec3(2.0, 4.0, 6.0)
+        val expected = Vec3(1.0, 2.0, 3.0)
+        assertEquals(expected, u / 2.0)
+    }
+    @Test
+    fun testDot() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        assertEquals(32.0, u.dot(v))
+    }
+    @Test
+    fun testCross() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val v = Vec3(4.0, 5.0, 6.0)
+        val expected = Vec3(-3.0, 6.0, -3.0)
+        assertEquals(expected, u.cross(v))
+    }
+    @Test
+    fun testUnitVector() {
+        val u = Vec3(1.0, 2.0, 3.0)
+        val expected = Vec3(0.2672612419124244,0.5345224838248488, 0.8017837257372732)
+        assertEquals(expected, u.unitVector())
+    }
+}
